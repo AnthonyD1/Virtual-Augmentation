4e2b7699e495bc0fd548c46b00a9cbb28b27d76e
Joseph Tortorello <32490063+NotTheRealJoe@users.noreply.github.com>
2018-03-29 13:18:18 -0500

    Added argument to virtual machine script to enable audio

--- developer_resources/start-vm.sh
+++ developer_resources/start-vm.sh
@@ -5,6 +5,7 @@
 # You can set the amount of RAM in megabytes here:
 RAM=2048
 
+QEMU_AUDIO_DRV=spice
 qemu-system-x86_64 \
 	-drive media=disk,file="$1",format=qcow2,index=0 \
 	-m $RAM \
@@ -14,4 +15,5 @@ qemu-system-x86_64 \
 	-vga qxl \
 	-spice port=5900,addr=127.0.0.1,disable-ticketing \
 	-device virtio-serial-pci \
-	-device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent
+	-device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent \
+	-device AC97,id=sound0,bus=pci.0,addr=0x4
