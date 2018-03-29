a10f69be1c147234309b97cdacdfb1e180d1e4be
Joseph Tortorello <32490063+NotTheRealJoe@users.noreply.github.com>
2018-03-29 13:12:19 -0500

    Altered the start vm script to work right with the disk image

--- developer_resources/start-vm.sh
+++ developer_resources/start-vm.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # This script will start up the virtual machine development environment
+# Pass the path to the disk image as the first command-line argument
 # You need to make sure VT-x or AMD-V is enabled in your BIOS settings
 # You can set the amount of RAM in megabytes here:
 RAM=2048
 
-qemu-system-x86_64 -drive media=disk,file=HoloLensDev.qcow2,format=qcow2,index=0 -m $RAM -cpu host --enable-kvm -usbdevice tablet -boot d
+qemu-system-x86_64 \
+	-drive media=disk,file="$1",format=qcow2,index=0 \
+	-m $RAM \
+	-cpu host \
+	--enable-kvm \
+	-usbdevice tablet \
+	-vga qxl \
+	-spice port=5900,addr=127.0.0.1,disable-ticketing \
+	-device virtio-serial-pci \
+	-device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent
