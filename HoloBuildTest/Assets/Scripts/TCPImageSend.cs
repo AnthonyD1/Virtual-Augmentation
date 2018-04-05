83e4f14008ce77f23bc05b003b052f4d242e464e
nottherealjoe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-04-05 14:05:31 -0500

    Forget what I changed, removing it from project anyways

--- HoloBuildTest/Assets/Scripts/TCPImageSend.cs
+++ HoloBuildTest/Assets/Scripts/TCPImageSend.cs
@@ -9,7 +9,9 @@ using System.Net.Sockets;
 using System.IO;
 using System.Text;
 using System.Threading;
-using 
+#if !UNITY_EDITOR
+    using Windows.Networking.Sockets.StreamSocket;
+#endif
 
 public class TCPImageSend : MonoBehaviour {
 
@@ -24,7 +26,11 @@ public class TCPImageSend : MonoBehaviour {
 
     //Internal use
     private IPEndPoint ipEndpoint;
-    private Windows.Networking.Sockets.StreamSocket sock = new Windows.Networking.Sockets.StreamSocket();
+    #if UNITY_EDITOR
+        private Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    #else
+        private Windows.Networking.Sockets.StreamSocket sock = new Windows.Networking.Sockets.StreamSocket();
+    #endif
 
     // Use this for initialization
     void Start () {
@@ -37,49 +43,52 @@ public class TCPImageSend : MonoBehaviour {
         }
     }
 
-    private async void Connect(string host, string port) {
-        try {
-            if (exchangeTask != null) StopExchange();
-
-            
-            Windows.Networking.HostName serverHost = new Windows.Networking.HostName(host);
-            await socket.ConnectAsync(serverHost, port);
-
-            Stream streamOut = socket.OutputStream.AsStreamForWrite();
-            writer = new StreamWriter(streamOut) { AutoFlush = true };
-
-            Stream streamIn = socket.InputStream.AsStreamForRead();
-            reader = new StreamReader(streamIn);
-
-            RestartExchange();
-            successStatus = "Connected!";
-        } catch (Exception e) {
-            errorStatus = e.ToString();
-        }
-    }
-
     void Update () {
         //TODO: We can get the return code of this function and use it to check for errors
         SendVarData(sock, texture.EncodeToJPG());
 	}
 
-    //Taken from https://answers.unity.com/questions/230269/send-rendertexture-via-tcp-or-udp.html
-    private static int SendVarData(Socket s, byte[] data) {
-        int total = 0;
-        int size = data.Length;
-        int dataleft = size;
-        int sent;
+    #if UNITY_EDITOR
+        //Taken from https://answers.unity.com/questions/230269/send-rendertexture-via-tcp-or-udp.html
+        private static int SendVarData(Socket s, byte[] data) {
+            int total = 0;
+            int size = data.Length;
+            int dataleft = size;
+            int sent;
 
-        byte[] datasize = new byte[0];
-        datasize = BitConverter.GetBytes(size);
-        sent = s.Send(datasize);
+            byte[] datasize = new byte[0];
+            datasize = BitConverter.GetBytes(size);
+            sent = s.Send(datasize);
 
-        while (total < size) {
-            sent = s.Send(data, total, dataleft, SocketFlags.None);
-            total += sent;
-            dataleft -= sent;
+
+            while (total < size) {
+                sent = s.Send(data, total, dataleft, SocketFlags.None);
+                total += sent;
+                dataleft -= sent;
+            }
+
+            return total;
         }
+    #else
+        private async void ConnectUWP(string host, string port) {
+            try {
+                if (exchangeTask != null) StopExchange();
+        
+                socket = new Windows.Networking.Sockets.StreamSocket();
+                Windows.Networking.HostName serverHost = new Windows.Networking.HostName(host);
+                await socket.ConnectAsync(serverHost, port);
 
-        return total;
-    }
+                Stream streamOut = socket.OutputStream.AsStreamForWrite();
+                writer = new StreamWriter(streamOut) { AutoFlush = true };
+
+                Stream streamIn = socket.InputStream.AsStreamForRead();
+                reader = new StreamReader(streamIn);
+
+                RestartExchange();
+                successStatus = "Connected!";
+            } catch (Exception e) {
+                errorStatus = e.ToString();
+            }
+        }
+    #endif
 }
