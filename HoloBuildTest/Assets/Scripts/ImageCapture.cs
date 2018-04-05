6e2eddcef0070aa8978742856056f1cf16bac5b9
nottherealjoe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-04-04 22:28:22 -0500

    Started working on scripts for image capture script. Made script to change HUD text

---
+++ HoloBuildTest/Assets/Scripts/ImageCapture.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.XR.WSA.WebCam;
+
+public class ImageCapture : MonoBehaviour {
+    PhotoCapture photoCaptureObject = null;
+    Texture2D targetTexture = null;
+
+	// Use this for initialization
+	void Start () {
+        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
+            photoCaptureObject = captureObject;
+            CameraParameters cameraParameters = new CameraParameters();
+
+            cameraParameters.hologramOpacity = 0.0f;
+            cameraParameters.cameraResolutionWidth = cameraResolution.width;
+            cameraParameters.cameraResolutionHeight = cameraResolution.height;
+            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
+                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+            })
+        });
+	}
+
+    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame) {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ HoloBuildTest/Assets/Scripts/TextChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextChange : MonoBehaviour {
+
+    public Text PeopleCountText;
+    public string textValue;
+
+	// Use this for initialization
+	void Start () {
+        PeopleCountText.text = "Hello, World!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PeopleCountText.text = textValue;
+	}
+}
