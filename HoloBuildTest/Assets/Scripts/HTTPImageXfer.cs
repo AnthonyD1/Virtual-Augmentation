8201019f0f7c41c44b26c20632a2397d8f755c17
nottherealjoe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-04-05 14:16:50 -0500

    Added script for sending image data over HTTP

---
+++ HoloBuildTest/Assets/Scripts/HTTPImageXfer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HTTPImageXfer : MonoBehaviour {
+    public string serverAddress = "127.0.0.1";
+    public int serverPort = 80;
+
+	// Use this for initialization
+	void Start () {
+        //Nothing
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Nothing
+	}
+
+    public IEnumerator Post(byte[] jpegData) {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        //formData.Add(new MultipartFormFileSection("my file data", "myfile.txt"));
+        formData.Add(new MultipartFormDataSection(jpegData));
+
+        UnityWebRequest www = UnityWebRequest.Post("http://" + serverAddress + serverPort.ToString(), formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError) {
+            Debug.Log(www.error);
+        } else {
+            Debug.Log("Form upload complete!");
+        }
+    }
+}
