fe3075dc800bc37992e263f85e0ea6e6214f092a
Cole Horner <colehorner94@gmail.com>
2018-04-05 13:45:49 -0500

    Added dynamic text
    People in room increments on update, just to test and get it prepared for it to change based on people in room.

---
+++ HoloBuildTest/Assets/HUD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+    public Text People;
+
+    private int PeopleCount;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PeopleCount = 0;
+        People.text = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        SetPeopleText();
+        IncrementPeopleCount();
+	}
+
+    void SetPeopleText()
+    {
+        People.text = "People in room: " + PeopleCount;
+    }
+
+    void IncrementPeopleCount()
+    {
+        PeopleCount++;
+    }
+}
