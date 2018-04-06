9aae7dcab329a266af522e5ddeeeaeec043b58fa
nottherealjoe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-04-05 21:12:06 -0500

    Kept fucking around with image capture stuff

--- HoloBuildTest/Assets/Scripts/ImageCapture.cs
+++ HoloBuildTest/Assets/Scripts/ImageCapture.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 using System.Linq;
 using UnityEngine.XR.WSA.WebCam;
 
+using System.Threading;
+
 public class ImageCapture : MonoBehaviour {
     PhotoCapture photoCaptureObject = null;
     Texture2D targetTexture = null;
@@ -20,32 +22,84 @@ public class ImageCapture : MonoBehaviour {
         Debug.Log("Photo capture script started");
 
         cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        Debug.Log("ImageCapture.Start: Resolution set: " + cameraResolution.ToString());
+
         targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+        Debug.Log("ImageCapture.Start: targetTexture created");
+
+        photoCaptureObject = new PhotoCapture();
 
         PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
+            Debug.Log("ImageCapture.Start: PhotoCapture.CreateAsync started");
             photoCaptureObject = captureObject;
+            Debug.Log("ImageCapture.Start: photoCaptureObject = captureObject");
             cameraParameters = new CameraParameters();
+            Debug.Log("ImageCapture.Start: Created cameraParameters");
 
             cameraParameters.hologramOpacity = 0.0f;
-            cameraParameters.cameraResolutionWidth = cameraResolution.width;
-            cameraParameters.cameraResolutionHeight = cameraResolution.height;
-            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
-        });
+            Debug.Log("ImageCapture.Start: hologramOpacity set");
 
-        photoCaptureObject.StartPhotoModeAsync(cameraParameters, OnPhotoModeStarted);
+            cameraParameters.cameraResolutionWidth = cameraResolution.width;
+            Debug.Log("ImageCapture.Start: camera resolution width set");
+
+            cameraParameters.cameraResolutionHeight = cameraResolution.height;
+            Debug.Log("ImageCapture.Start: camera resolution height set");
+
+            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+            Debug.Log("ImageCapture.Start: camera pixel format set. Async camera setup complete.");
+        });
+        
+        // Try the above without an inline async method
+        //PhotoCapture.CreateAsync(false, CameraSetup);
+        //CameraSetup(photoCaptureObject);
+
+        //Debug.Log("ImageCapture.Start: Camera setup async started");
 
         Debug.Log("Photo capture script Start() completed");
+
+        Application.Quit();
+
+        while (!photoCaptureModeOn) {
+            Debug.Log("ImageCapture.Start: Waiting for photo capture mode to be on...");
+        }
+
+        Capture();
+    }
+
+    void CameraSetup(PhotoCapture captureObject) {
+        photoCaptureObject = captureObject;
+        Debug.Log("CameraSetup: photoCaptureObject defined");
+
+        cameraParameters = new CameraParameters();
+        Debug.Log("CameraSetup: cameraParameters set up correctly");
+
+        cameraParameters.hologramOpacity = 0.0f;
+        Debug.Log("ImageCapture.CameraSetup: hologramOpacity set");
+        cameraParameters.cameraResolutionWidth = cameraResolution.width;
+        Debug.Log("ImageCapture.CameraSetup: Camera resolution (width) set");
+        cameraParameters.cameraResolutionHeight = cameraResolution.height;
+        Debug.Log("ImageCapture.CameraSetup: Camera resolution (height) set");
+        cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+        Debug.Log("ImageCapture.CameraSetup: pixelFormat set");
+
+        photoCaptureObject.StartPhotoModeAsync(cameraParameters, OnPhotoModeStarted);
+        Debug.Log("ImageCapture.CameraSetup: StartPhotoMode async started");
+
+        Debug.Log("ImageCapture.CameraSetup: Async camera setup completed");
     }
 
     void OnPhotoModeStarted(PhotoCapture.PhotoCaptureResult result) {
         photoCaptureModeOn = true;
-        Debug.Log("Photo capture mode is on");
+        Debug.Log("ImageCapture.OnPhotoModeStarted: Photo capture mode is on");
     }
 
     void Capture() {
         if (photoCaptureModeOn) {
             photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
             Debug.Log("Photo capture async job started");
+            photoCaptureModeOn = false;
+        } else {
+            Debug.Log("Photo not captured because photo capture mode is not on");
         }
     }
 
@@ -57,6 +111,7 @@ public class ImageCapture : MonoBehaviour {
 
         //Convert into jpeg data for sending over the network
         byte[] jpegData = targetTexture.EncodeToJPG();
+        Debug.Log("ImageCapture.OnCapturedPhotoToMemory: jpeg data is " + jpegData.ToString());
 
         //Get to the HTTPImageXfer script
         GameObject holoLensCamera = GameObject.Find("HoloLensCamera");
@@ -64,11 +119,14 @@ public class ImageCapture : MonoBehaviour {
 
         //Send the captured image as a Texture2D over to the TCPImageSend script for processing
         hTTPImageXfer.PostJpeg(jpegData);
+
+        photoCaptureModeOn = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
         //May want to make it not do it every single frame
-        Capture();
+        //Don't caputre every frame, just for now
+        //Capture();
 	}
 }
