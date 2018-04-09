66a4cd45bd0d58b09077dfe6bc3a43dbc2a55bea
Cole Horner <colehorner94@gmail.com>
2018-04-09 14:53:03 -0500

    Added ellipses
    Added Anaylsis Text
    Added ellipses after text to update

---
+++ HoloBuildTest/Assets/Scripts/Ellpises.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ellpises : MonoBehaviour {
+
+    public Text Analyzing;
+    public Plane Tint;
+	// Use this for initialization
+	void Start ()
+    {
+        /* Setting Text*/
+        Analyzing.text = "Analyzing   ";
+        /*Starts the invoke repeating method at second 0 and recalls it every second*/
+        InvokeRepeating("Ellipses", 0, 0.35f);
+        /* Used to cancel ellipses when triggered, must set a trigger before using or
+         * the ellipses won't work at all.
+        CancelInvoke();
+            GameObject.SetActive(false);
+            */
+        
+    }
+	
+	/*Checks how many periods the string has and adds another or removes all three*/
+	void Ellipses () {
+		if(Analyzing.text == "Analyzing   ")
+        {
+            Analyzing.text = "Analyzing.  ";
+        }
+        else if(Analyzing.text == "Analyzing.  ")
+        {
+            Analyzing.text = "Analyzing.. ";
+        }
+        else if(Analyzing.text == "Analyzing.. ")
+        {
+            Analyzing.text = "Analyzing...";
+        }
+        else if(Analyzing.text == "Analyzing...")
+        {
+            Analyzing.text = "Analyzing   ";
+        }
+    }
+}
