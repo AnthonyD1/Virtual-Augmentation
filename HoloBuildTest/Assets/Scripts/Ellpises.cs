edbdf7eb9e14f15be1327c877db5130d0dd48e1c
Cole Horner <colehorner94@gmail.com>
2018-04-09 20:53:48 -0500

    Minor tweaks
    Takes name as parameter if analyzing is turned off. Ideally would get a signal from server that a person is recognized, turn off analyzing and display name.

--- HoloBuildTest/Assets/Scripts/Ellpises.cs
+++ HoloBuildTest/Assets/Scripts/Ellpises.cs
@@ -6,21 +6,22 @@ using UnityEngine.UI;
 public class Ellpises : MonoBehaviour {
 
     public Text Analyzing;
+    public Text Name;
 	// Use this for initialization
 	void Start ()
     {
         /* Setting Text*/
         Analyzing.text = "Analyzing   ";
-        /*Starts the invoke repeating method at second 0 and recalls it every second*/
-        InvokeRepeating("Ellipses", 0, 0.35f);
+        /*Starts the invoke repeating method at second 0 and recalls it every .5 seconds*/
+        InvokeRepeating("Ellipses", 0, 0.5f);
         /* Used to cancel ellipses when triggered, must set a trigger before using or
          * the ellipses won't work at all. Change false to trigger if communication 
          * between hololens and server work. */
         if (false)
         {
-            Analyzing.text = "";
             CancelInvoke();
-            GameObject.Find("AnalyzingTint").SetActive(false);
+            GameObject.Find("Analyzing").SetActive(false);
+            NameDisplay("Bob");
         }
     }
 	
@@ -42,5 +43,12 @@ public class Ellpises : MonoBehaviour {
         {
             Analyzing.text = "Analyzing   ";
         }
+    }
+
+    /*Should take parameter name from server. Used to display name over the analyzing text
+     when a person is found. */
+    void NameDisplay(string name)
+    {
+        Name.text = name;
     }
 }
