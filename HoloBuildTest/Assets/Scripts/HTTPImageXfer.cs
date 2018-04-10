87fa6e2e3adde334b5be198232c3ba9f79d160b6
NotTheRealJoe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-04-09 21:34:10 -0500

    Kept fuckin around with HTTP stuff

--- HoloBuildTest/Assets/Scripts/HTTPImageXfer.cs
+++ HoloBuildTest/Assets/Scripts/HTTPImageXfer.cs
@@ -1,38 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
 using UnityEngine.Networking;
 
 public class HTTPImageXfer : MonoBehaviour {
     public string serverAddress;
     public string serverPath;
 
-    private TextChange TextChangeObject;
-    private UnityWebRequest unityWebRequestObject;
-    private int frameCount = 0;
 
-    private void Start() {
-        TextChangeObject = GameObject.Find("HoloLensCamera").GetComponent<TextChange>();
-        unityWebRequestObject = UnityWebRequest.Get("http://" + serverAddress + serverPath);
-        GetRequest();
-    }
-
-    private void Update() {
-        if(frameCount >= 150) {
-            GetRequest();
-            frameCount = 0;
-        }
-        frameCount++;
-
-        if(unityWebRequestObject.downloadHandler.isDone) {
-            GetCallback();
-        }
-    }
-
-    void GetRequest() {
-        unityWebRequestObject.SendWebRequest();
-    }
-
-    void GetCallback() {
-        TextChangeObject.SetTextValue(unityWebRequestObject.downloadHandler.text);
-        Debug.Log("HTTPImageXfer.GetTestCallback: Got content of " + unityWebRequestObject.downloadHandler.text);
-    }
 }
